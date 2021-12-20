fc3ce3884f7acbb10d2315161ad69376328dab4b
Max Henneberg <henneberg.maximilian@googlemail.com>
2021-12-20 22:48:08 +0100

    Moved spotifytoken.txt

--- src/spotifyController/SpotifyController.ts
+++ src/spotifyController/SpotifyController.ts
@@ -1,3 +1,13 @@
+import * as fs from 'fs';
+
 export class SpotifyController {
+    token: string;
+
+    constructor() {
+        this.token = fs.readFileSync('./spotifytoken.txt', 'utf-8');
+    }
+
+
+
 
 }
