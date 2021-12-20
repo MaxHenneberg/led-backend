d7964712791914fb4354233ff05954ade47dc436
Max Henneberg <henneberg.maximilian@googlemail.com>
2021-12-20 20:27:18 +0100

    Loop

--- src/main.js
+++ src/main.js
@@ -10,17 +10,22 @@ ws281x.configure(config);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    var pixels = new Uint32Array(5);
+    setInterval(loop(), 100);
+})
+
+
+offset = 0;
+function loop() {
+    var pixels = new Uint32Array(config.leds);
 
     // Create a fill color with red/green/blue.
     var red = 255, green = 0, blue = 0;
     var color = (red << 16) | (green << 8)| blue;
-
-    for (var i = 0; i < 5; i++)
-        pixels[i] = color;
+    offset = (offset+1)%config.leds;
+    pixels[offset] = color;
 
     ws281x.render(pixels);
-})
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
