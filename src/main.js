f2fb1fdb0b0b0fb4dc3a9870f71ecffa82a65e50
Max Henneberg <henneberg.maximilian@googlemail.com>
2021-12-20 19:54:40 +0100

    Changed config of lib

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ const express = require('express')
 const ws281x = require('rpi-ws281x')
 const app = express()
 const port = 3000
+config = {leds, brightness, gpio};
+this.config.leds = 5;
+this.config.brightness = 255;
+this.config.gpio = 18
+ws281x.configure(config);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    ws281x.configure({leds:16});
-    var pixels = new Uint32Array(16);
+    var pixels = new Uint32Array(5);
     ws281x.render(pixels);
 })
 
