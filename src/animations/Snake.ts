c8313e5082a39a99b6a127115784fd43530e1526
mhennebe <maximilian.a.hennebrg@capgemini.com>
2021-12-26 23:01:09 +0100

    Added Snake

--- src/animations/Snake.ts
+++ src/animations/Snake.ts
@@ -18,7 +18,7 @@ export class Snake extends LedAnimation {
         this.colorProgress = 0;
         this.progress = 0;
         this.direction = 1;
-        this.tailLength = 5;
+        this.tailLength = 15;
     }
 
     public play(): boolean {
